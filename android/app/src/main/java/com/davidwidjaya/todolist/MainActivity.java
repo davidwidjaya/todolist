058b1eb5c4a6a3168fe6b093ba09ff562ea5543a
davidwidjaya <davidpro71@gmail.com>
2022-05-08 23:09:51 +0700

    change appname & package name

--- android/app/src/main/java/com/davidwidjaya/todolist/MainActivity.java
+++ android/app/src/main/java/com/davidwidjaya/todolist/MainActivity.java
@@ -1,4 +1,4 @@
-package com.helloworld;
+package com.davidwidjaya.todolist;
 
 import com.facebook.react.ReactActivity;
 
@@ -16,7 +16,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "HelloWorld";
+    return "ToDoList";
   }
 
   /**
